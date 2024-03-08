ea010b9447cc19253de01349e0b55395caf886b1
Samuel Mensah <samuelmensqh@gmail.com>
2024-03-08 19:22:32 +0000

    New

--- src/api/fetchNews.tsx
+++ src/api/fetchNews.tsx
@@ -11,10 +11,14 @@ interface Article {
 
 export default async function fetchNews() {
     const API_KEY = "9e86051ea3494bb5b4443b14817a7ec3";
-    const q = "technology";
+
+    const qArray = ["technology", "coding", "software"];
+    const qLoop = Math.floor(Math.random() * qArray.length)
+    const q = qArray[qLoop];
+
     const URL = `https://newsapi.org/v2/everything?q=${q}&apiKey=${API_KEY}`;
     
-    const res = await fetch(URL, {next: {revalidate: 60000}});
+    const res = await fetch(URL);
     
     if (!res.ok) {
         throw new Error("Failed to fetch");
--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,89 +1,63 @@
-'use client'
+import { Box,Wrap,WrapItem, } from '@chakra-ui/react';
+import getVideos from '../../api/fetchVideos'
 
-import React, { useRef as UseRef, useState as UseState } from 'react'
-import { Box } from '@chakra-ui/react';
-import Image from 'next/image';
-import War from "../../../public/testImage/ukrainewar.webp";
+interface video {
+  id: {videoId: string} 
+  snippet: {
+    title: string
+    description: string
+    thumbnails: {
+      default:{
+        url: string
 
-export default function page() {
-  type vid = {id: number, url: string, title: string}
-  
-  
-  // Set hover play video
-  const [isPlaying, setIsPlaying] = UseState<{[key: number]:boolean}>({});
-  const videoRef = UseRef<{[key: number]: HTMLVideoElement | null}>({})
-
-  const handleMouseEnter = (id:number) => {
-    setIsPlaying((prevState)=> ({
-      ...prevState,
-      [id]: true
-    }));
-    if(videoRef.current[id]) {
-      videoRef.current[id]?.play();
+      }
     }
+  channelId: string
+  channelTitle: string
   }
-  
-  const handleMouseLeave = (id:number) => {
-    setIsPlaying((prevState)=> ({
-      ...prevState,
-      [id]: false
-    }))
-    if(videoRef.current[id]){
-      videoRef.current[id]?.pause();
-      videoRef.current[id]!.currentTime = 0;
-    }
-  }
+}
 
-  const vid: vid[] = [
-    {id: 1, url: '/videos/myvid.mp4', title: 'How I Create Snippets in Visual Studio Code'}, 
-    {id: 2, url: '/videos/myvid2.mp4', title: 'Build A Web3 Website In Less Than 10 Minutes'}, 
-    {id: 3, url: '/videos/myvid3.mp4', title: 'Problem Solving Techniques - For Programming Problems & Interviews'}, 
-  ]
+export default async function page() {
+  const data = await getVideos();
+  const videos: video[] = data.items;
+
   return (
     <div>
       <div className="px-4 md:px-8 mx-auto flex flex-col max-w-7xl">
         <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-8 mt-8">
-          {vid.map((i)=>(<Box className="max-w-[380px] w-full" key={i.id}>
-            <Box maxWidth={380} h={210} onMouseEnter={()=>handleMouseEnter(i.id)} onMouseLeave={()=>handleMouseLeave(i.id)}>
-            <video 
-            width={380} 
-            height={210} 
-            autoPlay={isPlaying[i.id]}
-            loop muted
-            className="rounded-xl cursor-pointer"
-            ref={(videoElement) => {
-              if (videoElement) {
-                videoRef.current[i.id] = videoElement;
-              }
-            }}
-            >
-                <source src={i.url}/>
-              </video>
+          {videos.map((i)=>(<Box className="max-w-[500px] w-full rounded-md" key={i.id.videoId}>
+            <Box maxWidth={500} h={200}>
+              <iframe
+              id={`youtube-${i.id.videoId}`}
+              src={`https://www.youtube.com/embed/${i.id.videoId}?modestbranding=1`}
+              allowFullScreen
+              frameBorder="0"
+              height="200px"
+              className="w-full"
+              />
             </Box>
-            <Box mt={2} mb={2}>
-              <Box>
+
+            <Box mt={1}>
                 <Box className="center-row-div justify-start">
-                  {/* avatar */}
-                  <Image
-                    src={War}
-                    alt=""
-                    width={16}
-                    height={16}
-                    className="bg-cover bg-center rounded-full w-8 h-8"
-                  />
-                  <h6>
-                    {i.title}
-                  </h6>
+                  <Wrap>
+                    <WrapItem>
+                      {/* <Avatar
+                        size="sm"
+                        src={i.thumbnails.default.url}
+                        name="Small Avatar"
+                        bg="pink.100"
+                      /> */}
+                    </WrapItem>
+                  </Wrap>
+                </Box>
+                <Box className=" text-base my-3">
+                  <h6 className="text-justify">{i.snippet.title.slice(0, 50)} ...</h6>
+                  <p className="mt-3 uppercase">
+                    {i.snippet.channelTitle}
+                  </p>
                 </Box>
               </Box>
-            </Box>
-            <Box display="flex" gap={4} fontSize={14}>
-              <p>techCode</p>
-              <p>•</p>
-              <p>8 mins ago</p>
-            </Box>
           </Box>))}
-
         </div>
       </div>
     </div>
