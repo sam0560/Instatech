8ddb007074f0925007c63768c404c1dbac8506c2
Samuel Mensah <samuelmensqh@gmail.com>
2024-03-28 16:19:58 +0000

    Fix implicit type error

--- src/app/home/News.tsx
+++ src/app/home/News.tsx
@@ -83,7 +83,7 @@ export default async function News() {
 
       {/* hero side news */}
       <div className="p-4 lg:p-0 lg:flex flex-col gap-4 flex md:hidden items-center">
-        {sideNews.map((item) => (
+        {sideNews.map((item:any) => (
           <div className="flex items-start gap-2" key={item.url}>
             {/* News items */}
             <Box w={120} h={70}>
