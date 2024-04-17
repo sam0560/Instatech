767bac0601b5595b29bdb660fb17075e9959eea7
Samuel Mensah <samuelmensqh@gmail.com>
2024-04-17 07:31:06 +0000

    logo name typo fix

--- src/app/component/NavMenu.tsx
+++ src/app/component/NavMenu.tsx
@@ -18,7 +18,7 @@ export default function NavMenu() {
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 relative" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link href="/home" className="-m-1.5 p-1.5 ">
-            <h3 className={climate_crisis.className}>Instatch.</h3>
+            <h3 className={climate_crisis.className}>Instatech</h3>
             
           </Link>
         </div>
